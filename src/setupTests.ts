2bb48f811bd3e9427cc018130d57a1e82e74175f
Qian chen <52212525+Az-qianchen@users.noreply.github.com>
2025-09-11 22:30:50 +0800

    test: mock canvas context

--- src/setupTests.ts
+++ src/setupTests.ts
@@ -1,2 +1,27 @@
 import '@testing-library/jest-dom';
 
+Object.defineProperty(HTMLCanvasElement.prototype, 'getContext', {
+  value: () => ({
+    fillRect: () => undefined,
+    clearRect: () => undefined,
+    getImageData: () => ({ data: [] }),
+    putImageData: () => undefined,
+    createImageData: () => ({ data: [] }),
+    setTransform: () => undefined,
+    drawImage: () => undefined,
+    save: () => undefined,
+    restore: () => undefined,
+    beginPath: () => undefined,
+    moveTo: () => undefined,
+    lineTo: () => undefined,
+    closePath: () => undefined,
+    stroke: () => undefined,
+    translate: () => undefined,
+    scale: () => undefined,
+    rotate: () => undefined,
+    arc: () => undefined,
+    fill: () => undefined,
+    measureText: () => ({ width: 0 }),
+  }),
+});
+
