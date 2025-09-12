21ecee5abeba10ae37d4e2c83aa783d2295b4a31
Qian chen <52212525+Az-qianchen@users.noreply.github.com>
2025-09-12 08:53:32 +0800

    test(setup): mock canvas context

--- src/setupTests.ts
+++ src/setupTests.ts
@@ -1,2 +1,9 @@
 import '@testing-library/jest-dom';
 
+Object.defineProperty(HTMLCanvasElement.prototype, 'getContext', {
+  configurable: true,
+  value: () => ({
+    measureText: () => ({ width: 0 } as TextMetrics),
+  }),
+});
+
